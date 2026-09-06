aef58b1ae00d7f2f4b36fe3a0c93796115a1af94
mcit39 <mattioc@protonmail.com>
2026-09-06 12:45:30 -0600

    Primer commit para receptor_uart

---
+++ src/design/receptor_uart.sv
@@ -0,0 +1,28 @@
+// Ver docs/diseño/modulos/M10_Receptor-UART.md
+module receptor_uart (
+  input logic clk,
+  input logic rst,
+  input logic [31:0] i_rdata, // lo que devuelve el periférico uart en la dirección que le estoy poniendo
+  input logic [2:0] i_state, // desde la fsm, de acá solo me interesa JUEGO
+
+  output logic [1:0] o_addr,
+  output logic o_write_enable,
+  output logic [31:0] o_wdata,
+  output logic [7:0] o_letra, // hacia REG_Letra-in
+  output logic o_valid_w // habilitación de carga de esa letra
+  );
+
+  // TODO: Revisar las direcciones cuando el frente de uart cierre el mapa de registros del periférico
+  localparam ADDR_CTRL = 2'b00;
+  localparam ADDR_DATOS_RX = 2'b10;
+  localparam BIT_NEW_RX = 1;
+
+  localparam JUEGO = 3'b010;
+
+  localparam ESPERA = 2'b00;
+  localparam LEE = 2'b01;
+  localparam LIMPIA = 2'b10;
+
+  logic [1:0] estado;
+
+endmodule
