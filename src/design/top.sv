28a097a3924fd52bcf59b77748b15a82524e1f25
Jefferson Chinchilla <jechinchilla@estudiantec.cr>
2026-09-13 13:44:58 -0600

    I/O Top_design

--- src/design/top.sv
+++ src/design/top.sv
@@ -1,6 +1,27 @@
-// Ver docs/diseño/planteamiento.md para la arquitectura completa (pendiente de escribir)
+
 module top (
-    input logic clk
+    input  logic       clk,
+    input  logic       rst,
+    input  logic       btn_sel,
+    input  logic       btn_ok,
+    //UART
+    input  logic       rx_i,
+
+    output logic       tx_o,
+    //LCD
+    output logic        lcd_rs_o,
+    output logic        lcd_rw_o,
+    output logic        lcd_e_o,
+    output logic [7:0]  lcd_data_o,
+
+    //7 segmentos
+    output logic [6:0]  seg,
+    output logic [3:0]  an,
+    output logic        dp,
+
+    //LEDs
+    output logic [1:0]  state_led,
+    output logic        buzzer
 );
 
 endmodule
